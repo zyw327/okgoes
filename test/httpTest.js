9259782dd5234b8cbc82973ebac2026cc92ff421
zengyuwen <327523057@qq.com>
2018-10-23 17:01:04 +0800

    update

--- test/httpTest.js
+++ test/httpTest.js
@@ -1,12 +1,12 @@
 const HttpClient = require('../lib/http/Client');
 const request = require('request');
-const host = "http://localhost:3002";
+const host = "http://localhost";
 
 let httpClient = new HttpClient();
 
-httpClient.setMethod(HttpClient.POST);
-httpClient.setParams({a: 2, b: {x: [1,3]}});
-httpClient.setUrl(host + '/index/api');
+httpClient.setMethod(HttpClient.GET);
+httpClient.setParams({phone: "123456", parkId: 9});
+httpClient.setUrl(host + '/user/validator');
 
 (async () => {
     // request('http://www.baidu.com', {json: true}, function(error, response, body) {
